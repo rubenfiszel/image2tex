f756c1d93192632dd13b9e7ac74c8fd0f2fe53ed
Ruben Fiszel <ruben.fiszel@epfl.ch>
2016-07-16 17:47:04 -0700

    ocr

--- ocr/recognition.py
+++ ocr/recognition.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 
-im = cv2.imread('example_text2.png')
+def read_img(filename):
+im = cv2.imread(filename)
 im3 = im.copy()
 
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -10,27 +11,25 @@ thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
 
 #################      Now finding Contours         ###################
 
-_, contours,_ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+_, contours,_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 samples =  np.empty((0,100))
-responses = []
-keys = [i for i in range(48,58)]
 
 for cnt in contours:
-    if cv2.contourArea(cnt)>50:
-        [x,y,w,h] = cv2.boundingRect(cnt)
+    [x,y,w,h] = cv2.boundingRect(cnt)
 
-        if  h>28:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
-            roi = thresh[y:y+h,x:x+w]
-            roismall = cv2.resize(roi,(10,10))
-            cv2.imshow('norm',im)
-            sample = roismall.reshape((1,100))
-            samples = np.append(samples,sample,0)
+    cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,123), 4)
+    roi = gray[y:y+h,x:x+w]
+#    roismall = cv2.resize(roi,(10,10))
 
-responses = np.array(responses,np.float32)
-responses = responses.reshape((responses.size,1))
-print "training complete"
+cv2.imshow('norm',im)
+key = cv2.waitKey(0)
 
-np.savetxt('generalsamples.data',samples)
-np.savetxt('generalresponses.data',responses)
+np.savetxt('symbol.data',samples)
+
+
+def main():
+    read_img('example_text.png')
+
+if __name__ == "__main__":
+    main()
