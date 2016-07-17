35436ba4467cfc1341eeea2234cb369cb76e06c6
Ruben Fiszel <ruben.fiszel@epfl.ch>
2016-07-17 09:53:55 -0700

    update

--- ocr/recognition.py
+++ ocr/recognition.py
@@ -1,7 +1,6 @@
 from read_img import read_img
-
-def regulize(im_list):
-    return [im for im in im_list]
+from sys import argv
+from regularize_image import regularize_image
 
 def nn_reco(im_list):
     return ["A" for im in im_list]
@@ -9,13 +8,21 @@ def nn_reco(im_list):
 def parse_with_dep(info_list):
     return 1
 
-def main():
-    (im_list, sp_list) = read_img('example_text.png')
-    regulized = regulize(im_list)
-    recognized = nn_reco(regulized)
+def process_image_to_symbol_map(path_to_image):
+    (im_list, sp_list) = read_img(path_to_image)
+    regularized = [
+        regularize_image(im)
+        for im in im_list
+    ]
+   
+    recognized = nn_reco(regularized)
     nn_input = zip(recognized, sp_list)
     tree = parse_with_dep(nn_input)
     print nn_input
 
 if __name__ == "__main__":
-    main()
+    if len(argv) == 2:
+        process_image_to_symbol_map(argv[1])
+    else:
+        print "Using default image"
+        process_image_to_symbol_map('example_text.png')
--- ocr/regularize_image.py
+++ ocr/regularize_image.py
@@ -1,18 +1,38 @@
 import numpy as np
-from scipy.misc import imresize
-from resizeimage import resizeimage
+from cv2 import resize, imshow, waitKey, imwrite
 
-def regularize_image(bitmap, resized_shape=(28, 28), extra_padding=10):
+def pad_image(bitmap, extra_padding_percentage=10):
+
     shape = bitmap.shape
     max_dim = max(*shape)
 
-    bitmap = bitmap[:,:,0]
+    extra_padding = int(max_dim * extra_padding_percentage / float(100))
 
     padded = np.pad(
         bitmap,
         ((max_dim - shape[0]) + extra_padding,(max_dim - shape[1]) + extra_padding),
-        mode='constant'
+        mode='constant',
+        constant_values=255
     )
-
-
     return padded
+
+
+def regularize_image(bitmap):
+
+	show(bitmap)
+	padded = pad_image(bitmap)
+	print padded.shape
+	show(padded)
+	resized = resize(padded, (28, 28))
+	print resized.shape
+	show(resized)
+	return resized
+
+
+i = 0
+def show(img):
+	global i
+	# imshow("image", resize(img (img.shape[0] * 20, img.shape[1] * 20)))
+	# waitKey()
+	i += 1
+	imwrite(str(i) + ".png", img)
