b39a52ff4a59b25e087782aa5815183784e1b6b3
Joe Warren <jhwarren@gmail.com>
2016-07-16 20:15:53 -0700

    init, typo correction

--- model/model_train.py
+++ model/model_train.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
             featurewise_std_normalization=False,  # divide inputs by std of the dataset
             samplewise_std_normalization=False,  # divide each input by its std
             zca_whitening=False,  
-            zoom_range=0.2# apply ZCA whitening
+            zoom_range=0.2, # apply ZCA whitening
             rotation_range=0.2,  # randomly rotate images in the range (degrees, 0 to 180)
             width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
             height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
